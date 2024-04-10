5e5a3879eb77a0f0efd4b45aa6bec013b719fc3c
Chanyu Choung <GT007790@mobis-partners.com>
2024-04-10 10:12:30 -0400

    CC - reverting back to old CreateRoot

--- po_gui/src/index.js
+++ po_gui/src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { createRoot } from 'react-dom';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById("root")).render(
+// (CY): BrowserRouter for internal react routing
+ReactDOM.render(
   <BrowserRouter>
     <App />
-  </BrowserRouter>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
